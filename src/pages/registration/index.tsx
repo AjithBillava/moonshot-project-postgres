b0c55ab99366cad42ff30d7e040dec36bd9d119b
AjithB <ajith.b@peopleinteractive.in>
2024-04-08 17:54:01 +0530

    fixed token generation for user creation

--- src/pages/registration/index.tsx
+++ src/pages/registration/index.tsx
@@ -18,7 +18,7 @@ function RegistrationPage() {
 
   useEffect(() => {
     const { data } = userRoute;
-    console.log("🚀 ~ useEffect ~ userRoute:", userRoute.)
+    console.log("🚀 ~ useEffect ~ userRoute:")
 
     if (password.length > 8 && name.length !== 0) {
       if (userRoute.isError) {
